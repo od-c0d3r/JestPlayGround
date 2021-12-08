02e21da0ebcf7f174c91fe611c1336c8e757877b
od-c0d3r <omar_rashad@aol.com>
2021-12-08 05:23:15 +0200

    Test: test for reverse

--- files/scripts.js
+++ files/scripts.js
@@ -1,4 +1,8 @@
-export default function stringLength(str) {
+export function stringLength(str) {
   if (str.length >= 1 && str.length <= 10) return str.length;
   throw new Error();
+}
+
+export function reverseString(str) {
+    return str.split('').reverse().join('')
 }
--- files/scripts.test.js
+++ files/scripts.test.js
@@ -1,11 +1,17 @@
-import stringLength from './scripts';
+import * as Scripts from './scripts';
 
 test('returns string length', () => {
   const expected = 4;
-  const result = stringLength('test');
+  const result = Scripts.stringLength('test');
   expect(result).toEqual(expected);
 });
 
 test('throw an error when string less than 1 and more than 10', () => {
-  expect(() => stringLength('veryolongsting more than 10char')).toThrow();
+  expect(() => Scripts.stringLength('veryolongsting more than 10char')).toThrow();
+});
+
+test('returns reversed string', () => {
+  let expected = 'pilf'
+  let result = Scripts.reverseString('flip');
+  expect(result).toBe(expected);
 });
